753c72aaca3e7174edd94d1186de5345522ff511
Ayrton Weiss <ayrtonweiss@gmail.com>
2023-07-07 19:49:24 -0300

    M1L5T5 - Adding Mempool to the blockchain.  Created function to send transactions to the blockchain.

--- protochain/src/lib/blockchain.ts
+++ protochain/src/lib/blockchain.ts
@@ -9,14 +9,18 @@ import TransactionType from "./transactionType";
  */
 export default class Blockchain {
     blocks: Block[];
+    mempool: Transaction[];
     nextIndex: number = 0;
     static readonly DIFFICULTY_FACTOR = 5;
+    static readonly TX_PER_BLOCK = 2;
     static readonly MAX_DIFFICULTY = 62;
 
     /**
      * Creates a new Blockchain
      */
     constructor() {
+        this.mempool = [];
+
         this.blocks = [new Block({
             index: this.nextIndex,
             previousHash: "",
@@ -40,6 +44,23 @@ export default class Blockchain {
         return Math.ceil(this.blocks.length / Blockchain.DIFFICULTY_FACTOR);
     }
 
+    addTransaction(transaction: Transaction): Validation {
+        const validation = transaction.isValid();
+        if (!validation.success)
+            return new Validation(false, "Invalid tx: " + validation.message);
+
+        //check if there is already a transaction with the same hash in the blockchain.
+        if (this.blocks.some(b => b.transactions.some(tx => tx.hash === transaction.hash)))
+            return new Validation(false, "Duplicated tx in blockchain.");
+
+        //check if there is already a transaction with the same hash in the mempool.
+        if (this.mempool.some(tx => tx.hash === transaction.hash))
+            return new Validation(false, "Duplicated tx in mempool.");
+
+        this.mempool.push(transaction);
+        return new Validation(true, transaction.hash);
+    }
+
     /**
      * Add block into the blockchain
      * @param block block to be added
@@ -52,10 +73,24 @@ export default class Blockchain {
         if (!validation.success)
             return new Validation(false, `Invalid block: ${validation.message}`);
 
+
+        //get transactions from block, only transactions with fees.
+        const txs = block.transactions.filter(tx => tx.type !== TransactionType.FEE).map(tx => tx.hash);
+
+        //filter mempool for transactions that were included in the block.
+        const newMempool = this.mempool.filter(tx => !txs.includes(tx.hash));
+
+        //additional validation to avoid miners forging transactions
+        if (newMempool.length + txs.length !== this.mempool.length)
+            return new Validation(false, "Invalid tx in block: mempool");
+
+        //remove transactions from mempool because block is valid and it is going to be added to the blockchain.
+        this.mempool = newMempool;
+
         this.blocks.push(block);
         this.nextIndex++;
 
-        return new Validation();
+        return new Validation(true, block.hash);
     }
 
     /**
@@ -83,10 +118,13 @@ export default class Blockchain {
         return 1;
     }
 
-    getNextBlocks(): BlockInfo {
-        const transactions = [new Transaction({
-            data: new Date().toString()
-        } as Transaction)];
+    getNextBlocks(): BlockInfo | null {
+
+        //return null if there is no tx in mempool
+        if (!this.mempool || !this.mempool.length)
+            return null;
+
+        const transactions = this.mempool.slice(0, Blockchain.TX_PER_BLOCK);
 
         const difficulty = this.getDifficulty();
         const previousHash = this.getLastBlock().hash;
