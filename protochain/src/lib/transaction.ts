03118fa7266bb9e2f40a11a845a1616c68c18601
Ayrton Weiss <ayrtonweiss@gmail.com>
2023-07-11 21:28:41 -0300

    M1L7T2 - Starting implementing Transaction Output class

--- protochain/src/lib/transaction.ts
+++ protochain/src/lib/transaction.ts
@@ -1,8 +1,8 @@
 import TransactionType from "./transactionType"
 import sha256 from 'crypto-js/sha256';
 import Validation from "./validation";
-import Blockchain from "./blockchain";
 import TransactionInput from "./transactionInput";
+import TransactionOutput from "./transactionOutput";
 
 /**
  * Transaction class
@@ -11,39 +11,63 @@ export default class Transaction {
     type: TransactionType;
     timestamp: number;
     hash: string;
-    txInput: TransactionInput | undefined;
-    to: string;
+    txInputs: TransactionInput[] | undefined;
+    txOutputs: TransactionOutput[];
 
     constructor(tx?: Transaction) {
         this.type = tx?.type || TransactionType.REGULAR;
         this.timestamp = tx?.timestamp || Date.now();
-        this.to = tx?.to || "";
 
-        if (tx && tx.txInput)
-            this.txInput = new TransactionInput(tx.txInput);
-        else
-            this.txInput = undefined;
+        this.txInputs = tx?.txInputs
+            ? tx.txInputs.map(txi => new TransactionInput(txi))
+            : undefined;
+
+        this.txOutputs = tx?.txOutputs
+            ? tx.txOutputs.map(txo => new TransactionOutput(txo))
+            : [];
 
         this.hash = tx?.hash || this.getHash();
+
+        //update tx outputs with the hash of this transaction.
+        this.txOutputs.forEach((txo, index, arr) => arr[index].tx = this.hash);
     }
 
     getHash(): string {
-        const from = this.txInput ? this.txInput.getHash() : "";
-        return sha256(this.type + from + this.to + this.timestamp).toString();
+        const from = this.txInputs && this.txInputs.length
+            ? this.txInputs.map(txi => txi.signature).join(",")
+            : "";
+
+        const to = this.txOutputs && this.txOutputs.length
+            ? this.txOutputs.map(tx0 => tx0.getHash()).join(",")
+            : "";
+
+        return sha256(this.type + from + to + this.timestamp).toString();
     }
 
     isValid(): Validation {
         if (this.hash !== this.getHash())
             return new Validation(false, "Invalid hash.");
 
-        if (!this.to)
-            return new Validation(false, "Invalid to.");
+        if (!this.txOutputs || !this.txOutputs.length || this.txOutputs.map(txo => txo.isValid()).some(v => !v.success))
+            return new Validation(false, "Invalid TXO.");
 
-        if (this.txInput) {
-            const validation = this.txInput.isValid();
-            if (!validation.success)
-                return new Validation(false, `Invalid tx: ${validation.message}`);
+        if (this.txInputs && this.txInputs.length) {
+            const validations = this.txInputs.map(txi => txi.isValid()).filter(v => !v.success);
+            if (validations && validations.length) {
+                const message = validations.map(v => v.message).join(" ");
+                return new Validation(false, `Invalid tx: ${message}`);
+            }
+
+            const inputSum = this.txInputs.map(txi => txi.amount).reduce((a, b) => a + b, 0);
+            const outputSum = this.txInputs.map(txi => txi.amount).reduce((a, b) => a + b, 0);
+            if (inputSum < outputSum)
+                return new Validation(false, "Invalid tx: input amounts must be equals or greater than outputs amounts.");
         }
+
+        if (this.txOutputs.some(txo => txo.tx !== this.hash))
+            return new Validation(false, "Invalid TXO reference hash.");
+
+        //TODO: validar as taxas e recompensas quando tx.type === FEE
 
         return new Validation();
     }
